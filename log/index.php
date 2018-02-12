817ddf4659df0ccdbc051bc19aad800f6e33135d
kcrimi <kevincrimi@gmail.com>
2018-02-12 07:43:50 +0000

    Remove carriage return on log script

--- log/index.php
+++ log/index.php
@@ -3,7 +3,7 @@
 	date_default_timezone_set('America/New_York');
 	$date = date('Y-m-d H:i:s', time());
 	$request_body = file_get_contents('php://input');
-	$log_entry = $date . " " . $request_body . "\r\n";
+	$log_entry = $date . " " . $request_body . "\n";
 	fwrite($log_file, $log_entry);
 	fclose($log_file);
 	echo $log_entry;
