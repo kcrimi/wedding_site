5f8fb68e301bd7ab62f0b97ea30af2cd6f92413b
kcrimi <kevincrimi@gmail.com>
2018-03-12 23:40:59 -0400

    sort delinquent guests

--- js/status-check.js
+++ js/status-check.js
@@ -1,9 +1,7 @@
 ---
 ---
 $(document).ready(function() {
-  var list = $('#status-name-list');
-  var guests = [];
-  var events = [];
+  var groups = {};
   var WEDDING_BOT_URL = '{{ site.wedding-bot-url }}';
 
   var guestListRequest = new XMLHttpRequest();
@@ -11,19 +9,48 @@ $(document).ready(function() {
     if (guestListRequest.readyState === XMLHttpRequest.DONE) {
       $(".loader-container").addClass("hidden");
       if (guestListRequest.status === 200) {
-        guests = JSON.parse(guestListRequest.responseText);
+        var guests = JSON.parse(guestListRequest.responseText);
+        // Group the results
         guests.forEach(function(item) {
+          console.log(item.relationship);
+          if (!groups[item.relationship]) {
+            groups[item.relationship] = {
+              count: 0,
+              unrespondedCount: 0,
+              unresponded: []
+            };
+          }
+          groups[item.relationship].count += item.guests.length;
           if (item.needs_rsvp) {
-            var row = $("<li></li>");
-            row.text(item.name);
-            list.append(row);
+          groups[item.relationship].unrespondedCount += item.guests.length;
+            groups[item.relationship].unresponded.push(item);
           }
         });
+        updateUI();
       }
     }
   };
 
-  guestListRequest.open('GET', WEDDING_BOT_URL+'/guests', true);
+  var updateUI = function() {
+    var container = $('#container');
+    for (var key in groups) {
+      if (key != "null") {
+        var section = $("<div>", {class: "status-section"});
+        var countText = groups[key].unrespondedCount + " / " + groups[key].count;
+        section.append($("<h3></h3>", {class: "unstylized"}).text(key + " "+countText));
+        var list = $("<ul></ul>", {style:"text-align:left"});
+        groups[key].unresponded.forEach(function(item) {
+          list.append($("<li></li>").text(item.name));
+        })
+        var listContainer = $("<div></div>", {class:"detail-container row split-row"})
+        listContainer.append(list);
+        section.append(listContainer);
+        container.append(section);
+      }
+    }
+  }
+
+  guestListRequest.open('GET', WEDDING_BOT_URL+'/guests?includeRelationship=true', true);
   guestListRequest.send();
 
   $(".loader-container").removeClass("hidden");
